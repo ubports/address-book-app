33d39b2cb6440e2218c6c7b105144396b8dd3f48
Leo Arias <leo.arias@canonical.com>
2014-02-28 07:18:20 -0600

    Use dictionary literals.

--- tests/autopilot/address_book_app/tests/test_emulators.py
+++ tests/autopilot/address_book_app/tests/test_emulators.py
@@ -35,7 +35,7 @@ class ContactEditorTestCase(tests.AddressBookAppTestCase):
         self.assertEqual(test_form_values, form_values)
 
     def test_fill_form_with_unknown_field_must_raise_error(self):
-        test_form_values = dict(unknown='dummy')
+        test_form_values = {'unknown': 'dummy'}
 
         contact_editor = self.main_window.go_to_add_contact()
         error = self.assertRaises(
