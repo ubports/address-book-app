bce480260eb8b4d8c47b0d15d385406d2516cf89
Renato Araujo Oliveira Filho <renato.filho@canonical.com>
2015-05-12 12:43:59 -0300

    Added missing files.

---
+++ tests/autopilot/address_book_app/pages/_ab_contact_view_page.py
@@ -0,0 +1,28 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+#
+# Copyright (C) 2014 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+""" ContactViewPage emulator for Addressbook App tests """
+
+import logging
+import time
+
+import autopilot.logging
+import ubuntuuitoolkit
+
+from address_book_app import address_book
+
+class ABContactViewPage(address_book.ContactViewPage):
+    """Autopilot helper for the Contact View page."""
