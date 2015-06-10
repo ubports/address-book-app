c12f9b25ae07771a76066b1e2c8393a1cb88a394
Renato Araujo Oliveira Filho <renato.filho@canonical.com>
2015-06-10 16:05:20 -0300

    Added missing test file.

---
+++ tests/autopilot/address_book_app/tests/test_export_contact.py
@@ -0,0 +1,36 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+# Copyright 2013 Canonical
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+
+"""Tests for the AddressBook App"""
+
+from autopilot.matchers import Eventually
+from testtools.matchers import Equals
+from testtools import skipUnless
+
+from address_book_app.tests import AddressBookAppTestCase
+from address_book_app import helpers
+
+
+class TestExportContact(AddressBookAppTestCase):
+    """Tests the erxport contact features"""
+
+    PRELOAD_VCARD = True
+
+    def test_export_contacts(self):
+        contact_list = self.app.main_window.get_contact_list_page()
+        contacts = contact_list.select_many('ContactDelegate')
+
+        # select one contact
+        contact_list.select_contacts([1])
+
+        # click on export header action
+        self.app.main_window.click_action_button('share')
+
+        # expect that the share page appears
+        share_page = self.app.main_window.get_share_page()
+        self.assertThat(share_page.active, Eventually(Equals(True)))
+
