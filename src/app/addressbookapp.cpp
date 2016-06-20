da6048f890bdb8d25430cffe886fbf92fc262dcc
Renato Araujo Oliveira Filho <renato.filho@canonical.com>
2016-06-20 14:04:17 -0300

    Fixed typo.

--- src/app/addressbookapp.cpp
+++ src/app/addressbookapp.cpp
@@ -48,7 +48,7 @@ static void printUsage(const QStringList& arguments)
              << "[addressbook:///create?phone=<phone-number>]"
              << "[addressbook:///pick?single=<true/false>]"
              << "[addressbook:///importvcard?url=<vcard-file>]"
-             << "[addressbook:///createAccount]""
+             << "[addressbook:///createAccount]"
              << "[--fullscreen]"
              << "[--help]"
              << "[-testability]";
